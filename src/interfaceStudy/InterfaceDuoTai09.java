7031e7217ad7b2f5959b0da9ca3ae70f2937a4b0
logicding <logicding@163.com>
2017-10-02 23:09:57 +0800

    多态特性和产生条件

---
+++ src/interfaceStudy/InterfaceDuoTai09.java
@@ -0,0 +1,20 @@
+package interfaceStudy;
+
+public class InterfaceDuoTai09 {
+/*
+ * 多态特性：
+ * 1.指的是运行期间动态绑定所指向的引用的类，允许不同的类对统一个消息做不同的响应，多台可以降低耦合性
+ * 2.父类引用指向了子类
+ * 3.有可扩充性
+ * 4.对修改是关闭的，对扩展是开放的
+ * 5.简化性，软件修改和编写
+ * 
+ * 多态产生的条件
+ * 1.继承或者实现接口  2.要有方法的覆盖和重写3.父类引用指向子类对象
+ * */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
