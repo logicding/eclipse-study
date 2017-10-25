005040882419bbc2a16383dfe6d75b26cd98f15b
logicding <logicding@163.com>
2017-10-26 00:21:30 +0800

    1.文件IO流（未完）
    不能把本地的.calsspath和另外一个文件给checkout了，因为那两个文件记录了build
    config，今天把那两个文件回退后无法编译运行了，报错找不到主方法，后来又重新设置了build path连接的library

---
+++ src/javaiostream/Gb.java
@@ -0,0 +1,13 @@
+package javaiostream;
+
+import java.io.File;
+
+public class Gb {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        File j = new File("C:\\Users\\Administrator\\Desktop\\kk.txt");
+        System.out.println(j.exists());
+	}
+
+}
