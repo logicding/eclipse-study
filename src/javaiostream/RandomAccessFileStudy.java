3a1ee4d6b8c76f10b145df953f28eedb33cf4423
logicding <logicding@163.com>
2017-11-08 20:46:59 +0800

    随机访问文件
    通过seek 或者skipbyte来移动文件指针 来访问不同的数据 int double ,要想直接访问double就要先跳过4个字节

---
+++ src/javaiostream/RandomAccessFileStudy.java
@@ -0,0 +1,66 @@
+package javaiostream;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class RandomAccessFileStudy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		write();
+		read();
+	}
+	private static void write() {
+		// TODO Auto-generated method st
+		RandomAccessFile raf  = null;
+		try {
+			raf = new RandomAccessFile("demo.dat", "rw");
+			raf.writeInt(100);
+			raf.writeBoolean(true);
+			raf.writeChar('A');
+			raf.writeUTF("java");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally{
+			if(raf != null){
+				try {
+					raf.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
+	private static void read() {
+		// TODO Auto-generated method stub
+		RandomAccessFile raf  = null;
+		try {
+			raf = new RandomAccessFile("demo.dat", "rw");
+			raf.seek(4);
+//			System.out.println(raf.readInt());
+			System.out.println(raf.readBoolean());
+//			raf.writeChar('A');
+//			raf.seek(10);
+			raf.skipBytes(2);
+//			raf.seek(1);
+			System.out.println(raf.readUTF());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally{
+			if(raf != null){
+				try {
+					raf.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+}
