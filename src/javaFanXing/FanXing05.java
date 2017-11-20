e73d5fbb8cc1dc653ef24bbb5d79b82f8ef1d781
logicding <logicding@163.com>
2017-11-20 20:48:16 +0800

    虚拟机处理泛型代码的时候：擦除过程->补偿和翻译

---
+++ src/javaFanXing/FanXing05.java
@@ -0,0 +1,68 @@
+package javaFanXing;
+
+public class FanXing05 {
+//泛型代码和虚拟机处理机制
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//补偿和翻译
+		PairOne po = new PairOne("tom");
+		String str = (String)po.getObj();
+		System.out.println(str);
+	}
+
+}
+//虚拟机中是没有泛型对象的
+
+
+//擦除过程
+class PairOne{
+	private Object obj;
+
+	/**
+	 * @return the obj
+	 */
+	public Object getObj() {
+		return obj;
+	}
+
+	/**
+	 * @param obj the obj to set
+	 */
+	public void setObj(Object obj) {
+		this.obj = obj;
+	}
+
+	public PairOne(Object obj) {
+		super();
+		this.obj = obj;
+	}
+	public showObj(){
+		System.out.println(this.obj + ":" + this.obj.getClass().getName());
+	}
+}
+
+/*class PairOne<T>{
+	private T obj;
+
+	*//**
+	 * @return the obj
+	 *//*
+	public T getObj() {
+		return obj;
+	}
+
+	*//**
+	 * @param obj the obj to set
+	 *//*
+	public void setObj(T obj) {
+		this.obj = obj;
+	}
+
+	public PairOne(T obj) {
+		super();
+		this.obj = obj;
+	}
+	public showObj(){
+		System.out.println(this.obj + ":" + this.obj.getClass().getName());
+	}
+}*/
