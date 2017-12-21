3b77dbefd3ebaa86f2af4e99c6013ed6dd60d1e4
logicding <logicding@163.com>
2017-12-21 20:21:42 +0800

    线程的启动和停止

--- src/phtread/TestCreateThread02.java
+++ src/phtread/TestCreateThread02.java
@@ -5,24 +5,31 @@ public class TestCreateThread02 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		new Thread(new Runnable() {
-			
-			@Override
-			public void run() {
-				// TODO Auto-generated method stub
-				boolean flag = true;
-				int i = 0;
-				while (flag) {
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					System.out.println(Thread.currentThread().getName()+" i= " + i++);
-				}
-			}
-		}).start();
+		Thread t = new Thread(new MyThread());
+//		t.run();//这个只是单线程了
+		t.start();
+		
+		while(true){
+			System.out.println("--------------");
+		}
+//		new Thread(new Runnable() {
+//			
+//			@Override
+//			public void run() {
+//				// TODO Auto-generated method stub
+//				boolean flag = true;
+//				int i = 0;
+//				while (flag) {
+//					try {
+//						Thread.sleep(1000);
+//					} catch (InterruptedException e) {
+//						// TODO Auto-generated catch block
+//						e.printStackTrace();
+//					}
+//					System.out.println(Thread.currentThread().getName()+" i= " + i++);
+//				}
+//			}
+//		}).start();
 		
 		
 		//内部类的方式
@@ -43,6 +50,24 @@ public class TestCreateThread02 {
 //				}
 //			}
 //		}.start();
-//	}
+	}
 
+}
+
+class MyThread implements Runnable{
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		boolean flag = true;
+		int i = 0;
+		while (flag) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println(Thread.currentThread().getName()+" i= " + i++);
+		}
+	}
 }
