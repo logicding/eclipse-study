b791f9355fccb0f2c45bf2592f40afdc0330805e
logicding <logicding@163.com>
2018-02-02 23:19:12 +0800

    java反射机制
    获取对象的自描述类的三种方法
    获取自描述类后如何去访问方法和属性呢？待下次解析

--- src/reflectsys/ReflectSys.java
+++ src/reflectsys/ReflectSys.java
@@ -1,4 +1,4 @@
-package phtread;
+package reflectsys;
 
 public class ReflectSys {
 //java的反射机制
@@ -6,11 +6,31 @@ public class ReflectSys {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		getClass1();
+		getClass2();
+		getClass3();
 		
 	}
+	//推荐使用该方法来获取自描述，主要原因是该方法可以通过配置文件来修改代码逻辑的效果
+	private static void getClass3() {
+	// TODO Auto-generated method stub
+		Class cls = null;
+		try {
+			cls = Class.forName("reflectsys.Persons");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(cls.getName());
+}
+	private static void getClass2() {
+	// TODO Auto-generated method stub
+		Class cls = Persons.class;//通过.class属性
+		System.out.println(cls.getName());
+	
+}
 	static void getClass1(){
 		Persons p = new Persons();
-		Class cls = p.getClass();
+		Class cls = p.getClass();//所有的类都有一个自描述类
 		System.out.println(cls.getName());
 	}
 }
